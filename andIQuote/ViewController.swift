4ec6ef246cb9164e8f3bea401f748ad26fb87849
hectorsvill <hectorstevenvillasano@gmail.com>
2019-12-13 17:19:26 -0800

    Initial Commit

---
+++ andIQuote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  andIQuote
+//
+//  Created by Hector on 12/13/19.
+//  Copyright © 2019 Hector. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
