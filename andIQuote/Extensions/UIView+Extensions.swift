bd5f426bc42781476106b811e7256675ed1e20b5
hectorsvill <hectorstevenvillasano@gmail.com>
2020-01-16 14:41:50 -0800

    uiview+extensions

---
+++ andIQuote/Extensions/UIView+Extensions.swift
@@ -0,0 +1,44 @@
+//
+//  UIView+screenShot.swift
+//  andIQuote
+//
+//  Created by Hector on 1/5/20.
+//  Copyright © 2020 Hector. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func screenShot() -> UIImage{
+        let render = UIGraphicsImageRenderer(size: bounds.size)
+        return render.image { _ in
+            drawHierarchy(in: bounds, afterScreenUpdates: true)
+        }
+    }
+    
+    func setBackground(to backgroundString: String) {
+        if backgroundString == "systemBackground" {
+            backgroundColor = .systemBackground
+        } else if backgroundString ==  "red" {
+            backgroundColor = .systemRed
+        } else if backgroundString == "green" {
+            backgroundColor = .systemGreen
+        } else if backgroundString ==  "blue" {
+            backgroundColor = .systemBlue
+        } else if backgroundString ==  "gray" {
+            backgroundColor = .systemGray
+        } else if backgroundString ==  "pink" {
+            backgroundColor = .systemPink
+        }else if backgroundString ==  "teal" {
+            backgroundColor = .systemTeal
+        } else if backgroundString == "indigo" {
+            backgroundColor = .systemIndigo
+        } else if backgroundString ==  "orange" {
+            backgroundColor = .systemOrange
+        } else if backgroundString == "yellow" {
+            backgroundColor = .systemYellow
+        } else if backgroundString ==  "purple" {
+            backgroundColor = .systemPurple
+        }
+    }
+}
