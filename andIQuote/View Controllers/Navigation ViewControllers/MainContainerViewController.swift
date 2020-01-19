f19f5b82ffe2c14be77c381761af9f147fdcfbcf
hectorsvill <hectorstevenvillasano@gmail.com>
2020-01-18 17:56:07 -0800

    slideoutmenu

---
+++ andIQuote/View Controllers/Navigation ViewControllers/MainContainerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainContainerViewController.swift
+//  andIQuote
+//
+//  Created by s on 1/18/20.
+//  Copyright © 2020 Hector. All rights reserved.
+//
+
+import UIKit
+
+class MainContainerViewController: UIViewController {
+    var navigationIsExpanded = false
+    var MainContainerViewController: MainContainerViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
