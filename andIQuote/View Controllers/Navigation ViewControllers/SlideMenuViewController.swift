b7d7bdcd9fd8887773f87bb3cf0bf76c640f18e0
hectorsvill <hectorstevenvillasano@gmail.com>
2020-01-19 16:44:19 -0800

    configureHierarchy

--- andIQuote/View Controllers/Navigation ViewControllers/SlideMenuViewController.swift
+++ andIQuote/View Controllers/Navigation ViewControllers/SlideMenuViewController.swift
@@ -20,19 +20,17 @@ class SlideMenuViewController: UIViewController {
     
     var dataSource: dataSouceDiffable!
     var collectionView: UICollectionView!
-    
     var quoteController: QuoteController!
-    var tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureHierarchy()
     }
     
 }
 
 extension SlideMenuViewController {
-    private func createLayout() -> UICollectionViewLayout {
+    private func createMainLayout() -> UICollectionViewLayout {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(80))
@@ -41,4 +39,17 @@ extension SlideMenuViewController {
         let layout = UICollectionViewCompositionalLayout(section: section)
         return layout
     }
+    
+    private func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createMainLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MenuCell")
+        view.addSubview((collectionView))
+        collectionView.delegate = self
+    }
+}
+
+extension SlideMenuViewController: UICollectionViewDelegate {
+    
 }
