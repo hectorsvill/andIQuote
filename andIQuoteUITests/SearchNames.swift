c2706b87a78ef9ee2171dcb620e137bc1aebcc69
hectorsvill <hectorstevenvillasano@gmail.com>
2020-08-05 10:05:35 -0700

    SearchNames enum

---
+++ andIQuoteUITests/SearchNames.swift
@@ -0,0 +1,15 @@
+//
+//  SearchNames.swift
+//  andIQuoteUITests
+//
+//  Created by Hector Villasano on 8/5/20.
+//  Copyright © 2020 Hector. All rights reserved.
+//
+
+import Foundation
+
+
+enum SearchNames: String, CaseIterable {
+    case Bruce_Lee = "Bruce Lee"
+    case Buddha = "Buddha"
+}
