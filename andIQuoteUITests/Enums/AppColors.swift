c0329ef433110daa56600b28e7776866d0e16a21
hectorsvill <hectorstevenvillasano@gmail.com>
2020-08-06 08:35:09 -0700

    Enums Folder

---
+++ andIQuoteUITests/Enums/AppColors.swift
@@ -0,0 +1,23 @@
+//
+//  AppColors.swift
+//  andIQuoteUITests
+//
+//  Created by Hector Villasano on 8/6/20.
+//  Copyright © 2020 Hector. All rights reserved.
+//
+
+import Foundation
+
+enum AppColors: String, CaseIterable {
+    case Systembackground = "Systembackground"
+    case Green = "Green"
+    case Blue = "Blue"
+    case Gray = "Gray"
+    case Pink = "Pink"
+    case Red = "Red"
+    case Teal = "Teal"
+    case Indigo = "Indigo"
+    case Orange = "Orange"
+    case Yellow = "Yellow"
+    case Purple = "Purple"
+}
